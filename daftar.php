0c0ebb05d092c3f779589314613d790ddb1a1d5e
Andre Christoga <andrechristoga@gmail.com>
2016-09-28 19:17:31 +0700

    Unfinished.

---
+++ daftar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" href="css/milligram.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="container" align="center">
+		<form action="faskes.php" method="get">
+			<label for="username">Nama:</label>
+			<input type="text" name="name"><br>
+			<label for="password">NIK:</label>
+			<input type="number" name="nik"><br>
+			<label for="password">No BPJS:</label>
+			<input type="number" name="bpjs"><br>
+			<input class="button button-outline" type="submit" value="Login">
+		</form>
+	</div>
+	<script src="js/main.js"></script>
+</body>
+</html>
---
+++ faskes.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" href="css/milligram.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<?php 
+		$nama = $_GET['name'];
+		$nik =  $_GET['nik'];
+		$bpjs = $_GET['bpjs'];
+	 ?>
+	<div class="container" align="center">
+		<div class="row">
+			<div class="column">
+				<h3>Nama Faskes</h3>
+				<p>
+					Lokasi: Jl. ... brRating: 3.5
+				</p>
+			</div>
+			<div class="column">
+				<h3>Nama Faskes</h3>
+				<p>
+					Lokasi: Jl. ... brRating: 3.5
+				</p>
+			</div>
+			<div class="column">
+				<h3>Nama Faskes</h3>
+				<p>
+					Lokasi: Jl. ... brRating: 3.5
+				</p>
+			</div>
+			<div class="column">
+				<h3>Nama Faskes</h3>
+				<p>
+					Lokasi: Jl. ... brRating: 3.5
+				</p>
+			</div>
+		</div>
+	</div>
+	<script src="js/main.js"></script>
+</body>
+</html>
